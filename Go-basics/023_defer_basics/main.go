f957800e4010f339432afb14fab8fdcbf2ff7e57
Ankit Verma <ankit8125@users.noreply.github.com>
2026-01-15 10:12:27 +0530

    complete git defer learning

--- Go-basics/023_defer_basics/main.go
+++ Go-basics/023_defer_basics/main.go
@@ -9,6 +9,15 @@ import (
 func main () {
 
 	// defer resp.body.Close()
+	fmt.Println("case 1: success")
+	if err := doWork(true); err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	fmt.Println("case 2: early fail")
+	if err := doWork(false); err != nil {
+		fmt.Println("error: ", err)
+	}
 }
 
 func doWork (success bool) error {
@@ -19,7 +28,8 @@ func doWork (success bool) error {
 
 	fmt.Println("start: resource acquired")
 
-	// 'defer' gurantees that this function runs at the end of the function
+	// 'defer' gurantees that this function runs at the end of the function, regardless of whether it exits normally or via an early return
+	// LIFO order: If you defer multiple functions, they execute in Last-In-First-Out order (reverse order of declaration)
 	defer fmt.Println("end: resource released") 
 
 	if !success {
