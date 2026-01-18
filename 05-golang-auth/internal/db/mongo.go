a88bc4b8ecff7f47ffc2d1f2f0399caf06151e05
Ankit Verma <ankit8125@users.noreply.github.com>
2026-01-19 04:59:41 +0530

    add mongo connection

--- 05-golang-auth/internal/db/mongo.go
+++ 05-golang-auth/internal/db/mongo.go
@@ -1,1 +1,37 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"go-auth/internal/config"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Mongo struct {
+	Client *mongo.Client
+	DB *mongo.Database
+}
+
+func Connect (ctx context.Context, cfg config.Config) (*Mongo, error) {
+	connectCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(cfg.MongoURI)
+
+	client, err := mongo.Connect(connectCtx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("Mongo connection failed: %w", err)
+	}
+
+	// We can add PING logic as well, if we want. 
+
+	database := client.Database(cfg.MongoDBName)
+
+	return &Mongo{
+		Client: client,
+		DB: database,
+	}, nil
+}
